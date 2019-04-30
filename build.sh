53f168321f4511088f985a45633cc4e3fc2d2593
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-04-30 17:59:30 +0100

    Add support for Java 12.

--- build.sh
+++ build.sh
@@ -6,20 +6,24 @@ rm -rf build/
 mkdir -p build/classes build/testclasses
 
 javac \
-	-cp .:lib/* \
+	--release 12 --enable-preview \
+	-cp .:lib/*:lib/log/* \
 	-d build/classes/ \
 	-sourcepath src/ `find src/* -name *.java`
 
 javac \
-	-cp .:build/classes/:lib/*:lib/test/* \
+	--release 12 --enable-preview \
+	-cp .:build/classes/:lib/*:lib/log/*:lib/test/* \
 	-d build/testclasses/ \
 	-sourcepath testsrc/ `find testsrc/* -name *.java`
 
 junit=lib/test/junit-platform-console-standalone-1.3.2.jar
 
-java -cp .:${junit}:build/classes/:build/testclasses/:lib/*:lib/test/* \
-	org.junit.platform.console.ConsoleLauncher \
-		--details tree \
-		--scan-classpath \
-		--include-classname '.*Test.*'
+java \
+	--enable-preview \
+	-cp .:${junit}:build/classes/:build/testclasses/:lib/*:lib/test/* \
+		org.junit.platform.console.ConsoleLauncher \
+			--details tree \
+			--scan-classpath \
+			--include-classname '.*Test.*'
 
