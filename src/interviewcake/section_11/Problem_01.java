02e41508863db931dc01784c575352dcd578c5a8
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-01 22:04:59 +0100

    Add Javadoc.

--- src/interviewcake/section_11/Problem_01.java
+++ src/interviewcake/section_11/Problem_01.java
@@ -3,6 +3,16 @@ package interviewcake.section_11;
 import annotation.Bitwise;
 
 class Problem_01 {
+    /**
+     Asymptotic analysis:
+     <ul>
+     <li>time_worst=O(n)
+     <li>space_worst=O(1)
+     </ul>
+
+     @param A an array with many duplicate integers and one unique integer
+     @return the unique integer in {@code A}
+     */
     @Bitwise
     int findUniqueId(int[] A) {
         int m = 0;
