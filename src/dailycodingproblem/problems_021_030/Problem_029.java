84c57028008a93fbd10d7f99710d50d0d776808d
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-04 22:13:19 +0100

    Add Problem #029 (Daily Coding Problem).

--- src/dailycodingproblem/problems_021_030/Problem_029.java
+++ src/dailycodingproblem/problems_021_030/Problem_029.java
@@ -2,8 +2,66 @@ package dailycodingproblem.problems_021_030;
 
 /**
  Problem #29 [Easy] - Amazon
- <p>{@link com.fundingcircle.RunLenEncoding}
  */
 class Problem_029 {
-    // TODO
+    /**
+     Asymptotic analysis:
+     <ul>
+     <li>time_worst=O(n)
+     <li>space_worst=O(n)
+     </ul>
+
+     @param in a string
+     @return the run-length encoding of {@code in}
+     */
+    String encode(String in) {
+        var out = new StringBuilder();
+        var curr = '\0';
+        var n = 0;
+        for (var c : in.toCharArray()) { // O(n)
+            if (c != curr) {
+                if (n > 0) {
+                    out.append((n == 1) ? curr : n + "" + curr);
+                }
+                curr = c;
+                n = 1;
+            } else {
+                n++;
+            }
+        }
+        if (n > 0) {
+            out.append((n == 1) ? curr : n + "" + curr);
+        }
+        return out.toString();
+    }
+
+    /**
+     Asymptotic analysis:
+     <ul>
+     <li>time_worst=O(mn)
+     <li>space_worst=O(n)
+     </ul>
+
+     @param in a run-length encoded string
+     @return the decoded version of {@code in}
+     */
+    String decode(String in) {
+        var num = new StringBuilder();
+        var out = new StringBuilder();
+        for (var c : in.toCharArray()) { // O(m)
+            if (c >= '0' && c <= '9') {
+                num.append(c);
+            } else {
+                if (num.length() == 0) {
+                    out.append(c);
+                } else {
+                    for (var i = 0; i < Integer.valueOf(num.toString()); i++) { // O(n)
+                        out.append(c);
+                    }
+                    num = new StringBuilder();
+                }
+            }
+        }
+        return out.toString();
+    }
 }
--- testsrc/unit/dailycodingproblem/problems_021_030/Test_029.java
+++ testsrc/unit/dailycodingproblem/problems_021_030/Test_029.java
@@ -1,85 +1,85 @@
-package com.fundingcircle;
+package dailycodingproblem.problems_021_030;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
-class RunLenEncodingTest {
-    private RunLenEncoding rle;
+class Test_029 {
+    private Problem_029 P;
 
     @BeforeEach
     void beforeEach() {
-        rle = new RunLenEncoding();
+        P = new Problem_029();
     }
 
     @Test
     void itEncodesAnEmptyString() {
-        assertEquals("", rle.encode(""));
+        assertEquals("", P.encode(""));
     }
 
     @Test
     void itEncodesAStringWithSingleCharacters() {
-        assertEquals("XYZ", rle.encode("XYZ"));
+        assertEquals("XYZ", P.encode("XYZ"));
     }
 
     @Test
     void itEncodesAStringWithoutSingleCharacters() {
-        assertEquals("2A3B4C", rle.encode("AABBBCCCC"));
+        assertEquals("2A3B4C", P.encode("AABBBCCCC"));
     }
 
     @Test
     void itEncodesAStringWithMixedCharacters() {
         var expected = "12WB12W3B24WB";
-        var actual = rle.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB");
+        var actual = P.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB");
         assertEquals(expected, actual);
     }
 
     @Test
     void itEncodesAStringWithWhitespace() {
-        assertEquals("2 hs2q q2w2 ", rle.encode("  hsqq qww  "));
+        assertEquals("2 hs2q q2w2 ", P.encode("  hsqq qww  "));
     }
 
     @Test
     void itEncodesALowercaseString() {
-        assertEquals("2a3b4c", rle.encode("aabbbcccc"));
+        assertEquals("2a3b4c", P.encode("aabbbcccc"));
     }
 
     @Test
     void itDecodesAnEmptyString() {
-        assertEquals("", rle.decode(""));
+        assertEquals("", P.decode(""));
     }
 
     @Test
     void itDecodesAStringWithSingleCharacters() {
-        assertEquals("XYZ", rle.decode("XYZ"));
+        assertEquals("XYZ", P.decode("XYZ"));
     }
 
     @Test
     void itDecodesAStringWithoutSingleCharacters() {
-        assertEquals("AABBBCCCC", rle.decode("2A3B4C"));
+        assertEquals("AABBBCCCC", P.decode("2A3B4C"));
     }
 
     @Test
     void itDecodesAStringWithMixedCharacters() {
         var expected = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB";
-        var actual = rle.decode("12WB12W3B24WB");
+        var actual = P.decode("12WB12W3B24WB");
         assertEquals(expected, actual);
     }
 
     @Test
     void itDecodesAStringWithWhitespace() {
-        assertEquals("  hsqq qww  ", rle.decode("2 hs2q q2w2 "));
+        assertEquals("  hsqq qww  ", P.decode("2 hs2q q2w2 "));
     }
 
     @Test
     void itDecodesALowercaseString() {
-        assertEquals("aabbbcccc", rle.decode("2a3b4c"));
+        assertEquals("aabbbcccc", P.decode("2a3b4c"));
     }
 
     @Test
     void itEncodesAndDecodesAString() {
         var s = "zzz ZZ  zZ";
-        assertEquals(s, rle.decode(rle.encode(s)));
+        assertEquals(s, P.decode(P.encode(s)));
     }
 }
