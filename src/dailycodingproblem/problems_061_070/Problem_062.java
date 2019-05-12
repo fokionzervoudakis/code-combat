9534fe44b41a17fc91dfd309a6f294ebf2c9ff38
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-12 22:58:04 +0100

    Add DP solutions.

--- src/dailycodingproblem/problems_061_070/Problem_062.java
+++ src/dailycodingproblem/problems_061_070/Problem_062.java
@@ -1,33 +1,106 @@
 package dailycodingproblem.problems_061_070;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  Problem #62 [Medium] - Facebook
  */
 class Problem_062 {
-    /**
-     Asymptotic analysis:
-     <ul>
-     <li>time_worst=O(mn)
-     <li>space_worst=O(n)
-     </ul>
+    class Rec {
+        /**
+         Asymptotic analysis:
+         <ul>
+         <li>time_worst=O(2^n)
+         <li>space_worst=O(n)
+         </ul>
 
-     @param r the number of rows in a matrix
-     @param c the number of columns in a matrix
-     @return the number of paths (moving left to right and top to bottom) from
-     the top-left corner to the bottom-right corner of a matrix with {@code r}
-     rows and {@code c} columns
-     */
-    int countWays(int r, int c) {
-        return helper(r, r, 0, 0);
+         @param m the number of rows in a matrix
+         @param n the number of columns in a matrix
+         @return the number of paths (moving left to right and top to bottom)
+         from the top-left corner to the bottom-right corner of a matrix with
+         {@code r} rows and {@code c} columns
+         */
+        int countPaths(int m, int n) {
+            return helper(m, n, 0, 0);
+        }
+
+        private int helper(int m, int n, int i, int j) {
+            if (i == m - 1 || j == n - 1) {
+                return 1;
+            } else {
+                int a = (i < m - 1) ? helper(m, n, i + 1, j) : 0;
+                int b = (j < n - 1) ? helper(m, n, i, j + 1) : 0;
+                return a + b;
+            }
+        }
     }
 
-    private int helper(int r, int c, int i, int j) {
-        if (i == r - 1 || j == c - 1) {
-            return 1;
-        } else {
-            int a = (i < r - 1) ? helper(r, c, i + 1, j) : 0;
-            int b = (j < c - 1) ? helper(r, c, i, j + 1) : 0;
-            return a + b;
+    class Mem {
+        Map<String, Integer> M = new HashMap<>();
+
+        /**
+         Asymptotic analysis:
+         <ul>
+         <li>time_worst=O(mn)
+         <li>space_worst=O(mn)
+         </ul>
+
+         @param m the number of rows in a matrix
+         @param n the number of columns in a matrix
+         @return the number of paths (moving left to right and top to bottom)
+         from the top-left corner to the bottom-right corner of a matrix with
+         {@code r} rows and {@code c} columns
+         */
+        int countPaths(int m, int n) {
+            return helper(m, n, 0, 0);
+        }
+
+        private int helper(int m, int n, int i, int j) {
+            String key = i + ":" + j;
+            if (M.containsKey(key)) {
+                return M.get(key);
+            } else {
+                int count;
+                if (i == m - 1 || j == n - 1) {
+                    count = 1;
+                } else {
+                    int a = (i < m - 1) ? helper(m, n, i + 1, j) : 0;
+                    int b = (j < n - 1) ? helper(m, n, i, j + 1) : 0;
+                    count = a + b;
+                }
+                M.put(key, count);
+                return count;
+            }
+        }
+    }
+
+    class Tab {
+        /**
+         Asymptotic analysis:
+         <ul>
+         <li>time_worst=O(mn)
+         <li>space_worst=O(mn)
+         </ul>
+
+         @param m the number of rows in a matrix
+         @param n the number of columns in a matrix
+         @return the number of paths (moving left to right and top to bottom)
+         from the top-left corner to the bottom-right corner of a matrix with
+         {@code r} rows and {@code c} columns
+         */
+        int countPaths(int m, int n) {
+            int[][] M = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == 0 || j == 0) {
+                        M[i][j] = 1;
+                    } else {
+                        M[i][j] = M[i][j - 1] + M[i - 1][j];
+                    }
+                }
+            }
+            return M[m - 1][n - 1];
         }
     }
 }
--- testsrc/unit/dailycodingproblem/problems_061_070/Test_062.java
+++ testsrc/unit/dailycodingproblem/problems_061_070/Test_062.java
@@ -1,31 +1,75 @@
 package dailycodingproblem.problems_061_070;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Test_062 {
-    private Problem_062 P;
+    @Nested
+    class WhenItUsesRecursion {
+        private Problem_062.Rec R;
 
-    @BeforeEach
-    void beforeEach() {
-        P = new Problem_062();
+        @BeforeEach
+        void beforeEach() {
+            R = new Problem_062().new Rec();
+        }
+
+        @Test
+        void itCountsPathsInASquareMatrix() {
+            assertEquals(1, R.countPaths(1, 1));
+            assertEquals(2, R.countPaths(2, 2));
+            assertEquals(6, R.countPaths(3, 3));
+            assertEquals(70, R.countPaths(5, 5));
+        }
+
+        @Test
+        void itCountsPathsInARectangularMatrix() {
+            assertEquals(1, R.countPaths(1, 2));
+            assertEquals(3, R.countPaths(2, 3));
+            assertEquals(10, R.countPaths(3, 4));
+            assertEquals(126, R.countPaths(5, 6));
+        }
     }
 
-    @Test
-    void itCountsPathsInASquareMatrix() {
-        assertEquals(1, P.countWays(1, 1));
-        assertEquals(2, P.countWays(2, 2));
-        assertEquals(6, P.countWays(3, 3));
-        assertEquals(70, P.countWays(5, 5));
+    @Nested
+    class WhenItUsesMemoization {
+        private Problem_062.Mem M;
+
+        @BeforeEach
+        void beforeEach() {
+            M = new Problem_062().new Mem();
+        }
+
+        @Test
+        void itCountsPathsInARectangularMatrix_5() {
+            assertEquals(1916797311, M.countPaths(51, 9));
+        }
+
+        @Test
+        void itCountsPathsInARectangularMatrix_6() {
+            assertEquals(193536720, M.countPaths(23, 12));
+        }
     }
 
-    @Test
-    void itCountsPathsInARectangularMatrix() {
-        assertEquals(1, P.countWays(1, 2));
-        assertEquals(3, P.countWays(2, 3));
-        assertEquals(10, P.countWays(3, 4));
-        assertEquals(126, P.countWays(5, 6));
+    @Nested
+    class WhenItUsesTabulation {
+        private Problem_062.Tab T;
+
+        @BeforeEach
+        void beforeEach() {
+            T = new Problem_062().new Tab();
+        }
+
+        @Test
+        void itCountsPathsInARectangularMatrix_5() {
+            assertEquals(1916797311, T.countPaths(51, 9));
+        }
+
+        @Test
+        void itCountsPathsInARectangularMatrix_6() {
+            assertEquals(193536720, T.countPaths(23, 12));
+        }
     }
 }
