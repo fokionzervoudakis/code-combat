39ca9b3e92029bce1a1aeb399f5e6105d97ffa32
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-15 13:50:32 +0100

    Add Problem #0242 (LeetCode).

---
+++ src/leetcode/problems_0241_0250/Problem_0242.java
@@ -0,0 +1,33 @@
+package leetcode.problems_0241_0250;
+
+import java.util.Arrays;
+
+class Problem_0242 {
+    /**
+     Asymptotic analysis:
+     <ul>
+     <li>time_worst=O(n log n)
+     <li>space_worst=O(n)
+     </ul>
+
+     @param s1 a string
+     @param s2 another string
+     @return true if {@code s1} and {@code s2} are anagrams, else false
+     */
+    // TODO test
+    boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char[] A = s1.toCharArray();
+        char[] B = s2.toCharArray();
+        Arrays.sort(A); // O(n log n)
+        Arrays.sort(B); // O(n log n)
+        for (int i = 0; i < A.length; i++) { // O(n)
+            if (A[i] != B[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
