d975e153475f8349c95f98000fdd09f73ff6e10e
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-16 10:23:38 +0100

    Update Javadoc.

--- src/dailycodingproblem/problems_051_060/Problem_051.java
+++ src/dailycodingproblem/problems_051_060/Problem_051.java
@@ -2,10 +2,8 @@ package dailycodingproblem.problems_051_060;
 
 /**
  Problem #51 [Medium] - Facebook
- <ul>
- <li>{@link interviewcake.section_03.Problem_05}
- <li>{@link interviewcake.section_12.Problem_03}
- </ul>
+ <p>{@link interviewcake.section_03.Problem_05}
+ <p>{@link interviewcake.section_12.Problem_03}
  */
 public class Problem_051 {
 }
--- src/dailycodingproblem/problems_051_060/Problem_059.java
+++ src/dailycodingproblem/problems_051_060/Problem_059.java
@@ -2,10 +2,8 @@ package dailycodingproblem.problems_051_060;
 
 /**
  Problem #59 [Hard] - Google
- <ul>
- <li>{@link interviewcake.section_02.Problem_05}
- <li>{@link interviewcake.section_09.Problem_03}
- </ul>
+ <p>{@link interviewcake.section_02.Problem_05}
+ <p>{@link interviewcake.section_09.Problem_03}
  */
 public class Problem_059 {
     // TODO
