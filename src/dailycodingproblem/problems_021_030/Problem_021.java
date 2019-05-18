0baa1489e497a5224c396ac3aa5be8e6edffa693
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-18 19:41:29 +0100

    Simplify implementation. Add comments.

--- src/dailycodingproblem/problems_021_030/Problem_021.java
+++ src/dailycodingproblem/problems_021_030/Problem_021.java
@@ -24,36 +24,31 @@ class Problem_021 {
             return A.length;
         }
 
-        var M1 = new HashMap<Integer, Integer>();
-        var M2 = new HashMap<Integer, Integer>();
+        var M = new HashMap<Integer, Integer>();
 
-        for (var L : A) {
-            M1.put(L.start, M1.containsKey(L.start) ? (M1.get(L.start) + 1) : 1);
-            M2.put(L.end, M2.containsKey(L.end) ? (M2.get(L.end) + 1) : 1);
+        for (var L : A) { // O(m)
+            M.put(L.start, M.containsKey(L.start) ? (M.get(L.start) + 1) : 1);
+            M.put(L.end, M.containsKey(L.end) ? (M.get(L.end) - 1) : -1);
         }
 
         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-        for (var L : A) {
+        for (var L : A) { // O(m)
             min = Math.min(min, L.start);
             max = Math.max(max, L.end);
         }
 
-        int lCount = 0, gCount = 0;
 
-        for (int i = min; i <= max; i++) {
-            if (M1.containsKey(i)) {
-                lCount += M1.get(i);
-            }
-            if (M2.containsKey(i)) {
-                lCount -= M2.get(i);
-            }
-            if (lCount > gCount) {
-                gCount = lCount;
+        int lMax = 0, gMax = 0;
+
+        for (int i = min; i <= max; i++) { // O(n)
+            if (M.containsKey(i)) {
+                lMax += M.get(i);
+                gMax = Math.max(gMax, lMax);
             }
         }
 
-        return gCount;
+        return gMax;
     }
 
     static class Lecture {
