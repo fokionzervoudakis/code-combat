42cc248354ed2ddfeec2d2a063cbc1b6918ea650
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-30 10:19:27 +0100

    Add Javadoc and unit tests.

--- src/leetcode/problems_0430_0440/Problem_0437.java
+++ src/leetcode/problems_0430_0440/Problem_0437.java
@@ -1,7 +1,17 @@
 package leetcode.problems_0430_0440;
 
-// TODO test
 class Problem_0437 {
+    /**
+     Asymptotic analysis:
+     <ul>
+     <li>time_worst=O(n^2)
+     <li>space_worst=O(n)
+     </ul>
+
+     @param N the root node
+     @param n the target sum
+     @return the number of paths in the tree with root {@code N} that sum to {@code n}
+     */
     int pathSum(Node N, int n) {
         if (N == null) {
             return 0;
@@ -9,7 +19,7 @@ class Problem_0437 {
         return helper(N, n) + pathSum(N.l, n) + pathSum(N.r, n);
     }
 
-    public int helper(Node N, int n) {
+    private int helper(Node N, int n) {
         if (N == null) {
             return 0;
         }
---
+++ testsrc/unit/leetcode/problems_0430_0440/Test_0437.java
@@ -0,0 +1,48 @@
+package leetcode.problems_0430_0440;
+
+import leetcode.problems_0430_0440.Problem_0437.Node;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Test_0437 {
+    private Problem_0437 P;
+
+    @BeforeEach
+    void beforeEach() {
+        P = new Problem_0437();
+    }
+
+    @Test
+    void itCountsZeroPaths() {
+        assertEquals(0, P.pathSum(null, 2));
+        assertEquals(0, P.pathSum(new Node(1), 2));
+    }
+
+    @Test
+    void itCountsOnePath() {
+        assertEquals(1, P.pathSum(new Node(1), 1));
+        assertEquals(1, P.pathSum(new Node(2), 2));
+    }
+
+    @Test
+    void itCountsManyPaths() {
+        var N = new Node(10);
+
+        N.l = new Node(5);
+        N.r = new Node(-3);
+
+        N.l.l = new Node(3);
+        N.l.r = new Node(2);
+
+        N.l.l.l = new Node(3);
+        N.l.l.r = new Node(-2);
+
+        N.l.r.r = new Node(1);
+
+        N.r.r = new Node(11);
+
+        assertEquals(3, P.pathSum(N, 8));
+    }
+}
