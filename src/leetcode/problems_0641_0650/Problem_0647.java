50009f3ca24738f8613aa372b2aea71877b17275
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-06-02 01:41:35 +0100

    Add Javadoc.

--- src/leetcode/problems_0641_0650/Problem_0647.java
+++ src/leetcode/problems_0641_0650/Problem_0647.java
@@ -1,11 +1,21 @@
 package leetcode.problems_0641_0650;
 
 class Problem_0647 {
+    /**
+     Asymptotic analysis:
+     <ul>
+     <li>time_worst=O(n^3)
+     <li>space_worst=O(1)
+     </ul>
+
+     @param str the target string
+     @return the number of palindromic substrings in {@code str}
+     */
     int countSubstrings(String str) {
         int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i; j < str.length(); j++) {
-                if (isPalindrome(str, i, j)) {
+        for (int i = 0; i < str.length(); i++) { // O(n)
+            for (int j = i; j < str.length(); j++) { // O(n)
+                if (isPalindrome(str, i, j)) { // O(n/2)
                     count++;
                 }
             }
@@ -14,7 +24,7 @@ class Problem_0647 {
     }
 
     private boolean isPalindrome(String str, int i, int j) {
-        while (i < j) {
+        while (i < j) { // O(n)
             if (str.charAt(i++) != str.charAt(j--)) {
                 return false;
             }
