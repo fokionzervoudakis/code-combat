9acd378d6a33d08141304f67b7ec5dff0ad2e2a9
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-06-04 12:49:32 +0100

    Rename method. Add test data.

--- testsrc/unit/leetcode/problems_0431_0440/Test_0438.java
+++ testsrc/unit/leetcode/problems_0431_0440/Test_0438.java
@@ -6,9 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Test_0438 {
     @Test
-    void itFindsAnagrams() {
+    void itFindsAllAnagrams() {
         var P = new Problem_0438();
         assertEquals("[0, 6]", P.findAnagrams("cbaebabacd", "abc").toString());
         assertEquals("[0, 1, 2]", P.findAnagrams("abab", "ab").toString());
+        assertEquals("[]", P.findAnagrams("", "a").toString());
+        assertEquals("[]", P.findAnagrams("aa", "bb").toString());
+        assertEquals("[]", P.findAnagrams("aaaaaaaaaa", "aaaaaaaaaaaaa").toString());
     }
 }
