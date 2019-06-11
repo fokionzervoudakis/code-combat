140b3f956a20a5494be5f09df600accf60b1ae5d
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-06-11 23:09:31 +0100

    Add partial implementation of Problem #092 (Daily Coding Problem).

--- src/dailycodingproblem/problems_091_100/Problem_092.java
+++ src/dailycodingproblem/problems_091_100/Problem_092.java
@@ -1,8 +1,45 @@
 package dailycodingproblem.problems_091_100;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  Problem #92 [Hard] - Airbnb
  */
 class Problem_092 {
-    // TODO
+    List<String> out = new ArrayList<>();
+    Set<String> done = new HashSet<>();
+
+    // TODO Javadoc+test
+    List<String> dfs(Map<String, List<String>> M) {
+        for (var E : M.entrySet()) {
+            if (!done.contains(E.getKey())) {
+                dfsVisit(M, E.getKey());
+            }
+        }
+        Collections.reverse(out);
+        return out;
+    }
+
+    private void dfsVisit(Map<String, List<String>> M, String start) {
+        var S = new Stack<String>();
+        S.push(start);
+        while (!S.isEmpty()) {
+            var str1 = S.pop();
+            out.add(str1);
+            done.add(str1);
+            for (var str2 : M.get(str1)) {
+                if (done.contains(str2)) {
+                    throw new RuntimeException();
+                } else {
+                    S.push(str2);
+                }
+            }
+        }
+    }
 }
