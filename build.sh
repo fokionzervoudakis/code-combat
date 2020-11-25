7c3a14fd6f81bb36be665ea8a08fd51563f84b11
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2020-11-25 19:13:52 +0000

    Update helper script.

--- build.sh
+++ build.sh
@@ -6,24 +6,21 @@ rm -rf build/
 mkdir -p build/classes build/testclasses
 
 javac \
-	--release 12 --enable-preview \
 	-cp .:lib/*:lib/log/* \
 	-d build/classes/ \
 	-sourcepath src/ `find src/* -name *.java`
 
 javac \
-	--release 12 --enable-preview \
 	-cp .:build/classes/:lib/*:lib/log/*:lib/test/* \
 	-d build/testclasses/ \
 	-sourcepath testsrc/ `find testsrc/* -name *.java`
 
 junit=lib/test/junit-platform-console-standalone-1.3.2.jar
 
-java \
-	--enable-preview \
-	-cp .:${junit}:build/classes/:build/testclasses/:lib/*:lib/test/* \
-		org.junit.platform.console.ConsoleLauncher \
-			--details tree \
-			--scan-classpath \
-			--include-classname '.*Test.*'
+# enable assertions
+java -ea -cp .:${junit}:build/classes/:build/testclasses/:lib/*:lib/test/* \
+	org.junit.platform.console.ConsoleLauncher \
+		--details tree \
+		--scan-classpath \
+		--include-classname '.*Test.*'
 
