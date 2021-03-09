fa9e7835d33fb28359b353f9fe842ae67ac3e30b
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2021-03-09 23:20:09 +0000

    Update classpaths.

--- build.sh
+++ build.sh
@@ -6,16 +6,14 @@ rm -rf build/
 mkdir -p build/classes build/testclasses
 
 javac \
-	-cp .:lib/*:lib/log/* \
+	-cp .:lib/* \
 	-d build/classes/ \
 	-sourcepath src/ `find src/* -name *.java`
 
 javac \
-	-cp .:build/classes/:lib/*:lib/log/*:lib/test/* \
+	-cp .:build/classes/:lib/*:lib/test/* \
 	-d build/testclasses/ \
 	-sourcepath testsrc/ `find testsrc/* -name *.java`
-
-junit=lib/test/junit-platform-console-standalone-1.3.2.jar
 
 # enable assertions
 java -ea -cp .:${junit}:build/classes/:build/testclasses/:lib/*:lib/test/* \
